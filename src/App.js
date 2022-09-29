aae4a92212bfff8e6f4f2e6cb4e980ede251b007
hemilrajpura-simform <hemil.r@simformsolutions.com>
2022-09-29 16:17:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import backgroundVideo from '../src/media/pexels-marian-croitoru-5607784.mp4';
+import sampleImage from '../src/media/pexels-taryn-elliott-4112237.jpg';
 import './App.css';
+import { BiBrightness,BiImageAdd } from 'react-icons/bi'
+import { BsDropletFill, BsFillMoonStarsFill, BsFillTriangleFill } from 'react-icons/bs'
+import { RiImageAddFill, RiTempHotFill } from 'react-icons/ri'
+import { useState } from 'react';
 
 function App() {
+  const [image,setImage] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video id="video" autoPlay loop muted>
+        <source src={backgroundVideo} type="video/mp4" />
+      </video>
+
+      <div className='toolMainSection'>
+        <div className='toolSectionContent'>
+          {/* <h1>LensArt</h1>  */}
+          <div className='ImageContainerSection'>
+            { image? <img src={image} alt={"Asa"} />:
+            <div className='AddImageSectionMain'>
+              <BiImageAdd />
+              <p>Choose an Image</p>
+            </div>
+           }
+          </div>
+
+          <div className='editingToolsSection'>
+            <div className='editingToolsList'>
+              <ul>
+                <li><BiBrightness /></li>
+                <li><BsDropletFill /></li>
+                <li><BsFillMoonStarsFill /></li>
+                <li><RiImageAddFill /></li>
+                <li><BsFillTriangleFill /></li>
+                <li><RiTempHotFill /></li>
+              </ul>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+      <a href='/hemilrajpura.in'><div className='creatorSectionMain'>
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/3f9052106543203.5f91fa2e4e14d.png" /><p>hemilrajpura.in</p>
+       </div></a>
     </div>
   );
 }
---
+++ src/Components/Slider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Slider = () => {
+  return (
+    <div className='SliderSection'>
+        <input type="range" />
+    </div>
+  )
+}
+
+export default Slider
